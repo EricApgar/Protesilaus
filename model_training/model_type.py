c9131ca862b35bf7dc735fb186cfe872ca2ae109
EricApgar <ericapgar1@gmail.com>
2020-01-31 22:54:50 -0600

    Rebnamed Folder.

---
+++ model_training/model_type.py
@@ -0,0 +1,11 @@
+# Description here.
+
+def calc_model_type(data_frame, truth_name):
+    
+    # Assumes data already been scrubbed:
+    # Data frame is homogenous data types per column.
+    # Really this is just a check to see if truth column is strings or numbers.
+    
+    a = 1
+
+    return a
