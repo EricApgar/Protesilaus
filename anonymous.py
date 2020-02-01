152f8fa3183bef02ab1d0b34f9ddc11156d25a75
EricApgar <ericapgar1@gmail.com>
2020-01-31 21:05:52 -0600

    Comments to the anonymous function for description and usage.

--- anonymous.py
+++ anonymous.py
@@ -1,3 +1,12 @@
+# This lets you create an object with a bunch of name-value pairs.
+# Example below. You pass it the property names and values and it will
+# let you refer to those properties with "." notation.
+
+# var = Anonymous(predictions=my_dict, 
+#                 train_time=5, 
+#                 accuracy=pandas.DataFrame())
+# var.train_time = 5
+
 class Anonymous(object):
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
